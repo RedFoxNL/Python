afa75df798dfea52353bcc5f26794d8520d4526b
RedFoxNL <thom.boer@gmail.com>
2017-09-22 11:56:52 +0200

    oefening 2 DNA string

---
+++ oefeningen/oefening 2.py
@@ -0,0 +1,19 @@
+# In deze oefening moet een genoom uit een string aangewezen worden.
+# Er zijn 4 stukken dna die uit de DNA string gehaald moeten worden. Dit wordt gedaan via replace.
+# Replace vervangt de waarde welke beschreven staan in de variabelen met de dnaReplace variabele
+
+dna = 'ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCCCCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCCTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGAAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCCCTGCAGGAACTTCTTCTGGAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAGTTTAATTACAGACCTGAA'
+
+dna1 = 'ATG'
+dna2 = 'TAG'
+dna3 = 'TGA'
+dna4 = 'TAA'
+
+dnaReplace1 = '*ATG*'
+dnaReplace2 = '*TAG*'
+dnaReplace3 = '*TGA*'
+dnaReplace4 = '*TAA*'
+
+newDNA = dna.replace(dna1, dnaReplace1).replace(dna2, dnaReplace2).replace(dna3, dnaReplace3).replace(dna4, dnaReplace4)
+
+print(newDNA)
