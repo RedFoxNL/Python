373792e4dda46f40282a522908b252c8b7783ec8
RedFoxNL <thom.boer@gmail.com>
2017-09-25 09:06:20 +0200

    oefening 2 DNA string

---
+++ oefeningen/oefening3.py
@@ -0,0 +1,10 @@
+# Opdracht 3A
+d = {'red':'#FF0000', 'green':'#008000', 'Black':'#000000', 'white':'#FFFFFF'}
+print(sorted(d))
+
+# Opdracht 3B
+d1 ={'a':100,'b':200}
+d2 ={'x':300, 'y':200}
+d3 = d2, d1
+print(d3)
+
