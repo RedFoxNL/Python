ad8f9dfa53f1403e4b7b6d110614c966272a2a8e
RedFoxNL <thom.boer@gmail.com>
2017-09-27 10:50:27 +0200

    Functie geschreven in oefeningen waar nog geen functies inzaten. Ook de map met oefeningen opgeschoond

--- oefeningen/oefening 2.py
+++ oefeningen/oefening 2.py
@@ -4,16 +4,17 @@
 
 dna = 'ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCCCCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCCTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGAAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCCCTGCAGGAACTTCTTCTGGAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAGTTTAATTACAGACCTGAA'
 
-dna1 = 'ATG'
-dna2 = 'TAG'
-dna3 = 'TGA'
-dna4 = 'TAA'
+def ondekDNA(arg):
+    dna = arg
+    dna1 = 'ATG'
+    dna2 = 'TAG'
+    dna3 = 'TGA'
+    dna4 = 'TAA'
+    dnaReplace1 = '*ATG*'
+    dnaReplace2 = '*TAG*'
+    dnaReplace3 = '*TGA*'
+    dnaReplace4 = '*TAA*'
+    newDNA = dna.replace(dna1, dnaReplace1).replace(dna2, dnaReplace2).replace(dna3, dnaReplace3).replace(dna4, dnaReplace4)
+    print(newDNA)
 
-dnaReplace1 = '*ATG*'
-dnaReplace2 = '*TAG*'
-dnaReplace3 = '*TGA*'
-dnaReplace4 = '*TAA*'
-
-newDNA = dna.replace(dna1, dnaReplace1).replace(dna2, dnaReplace2).replace(dna3, dnaReplace3).replace(dna4, dnaReplace4)
-
-print(newDNA)
+ondekDNA(dna)
