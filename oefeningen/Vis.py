8275be99f9735a8f1b05e8e3cc3e7eea0c0b50b7
RedFoxNL <thom.boer@gmail.com>
2017-10-02 11:32:09 +0200

    fout in dubbele counter

--- oefeningen/Vis.py
+++ oefeningen/Vis.py
@@ -1,26 +1,26 @@
 # Dit programma gaat over vissen in een aquarium.
 
-def main(id, soort, gewicht, lengte, kleur):
-    id = id
+def main(soort, gewicht, lengte, kleur):
     soort = soort
     gewicht = gewicht
     lengte = lengte
     kleur = kleur
 
     vis = []
-    vis.append(id, soort, gewicht, lengte, kleur)
-    print(vis)
+    aanmaakVis = [soort, gewicht, lengte, kleur]
+    vis.append(aanmaakVis)
     bijten(soort)
+    aqarium(lengte)
 
 # Deze functie gaat na of de vis een bijtende vis is of niet
 def bijten(soort):
     bijtVis = ['haaien','piranja','kreeft']
     for vis in bijtVis:
-        if soort == bijtVis:
-            print("Deze vis mag niet geplaatst worden bij de volgende vissen: ")
-            print(vis)
+        if soort in bijtVis:
+            print('----',vis)
         else:
             vegetarisch(soort)
+    print('\n')
 
 # Deze functie gaat na of een vis vegetarich is of niet. Zoja, wat eet deze vis?
 def vegetarisch(soort):
@@ -28,10 +28,9 @@ def vegetarisch(soort):
     soortVis = ['start index op 1',"Deze vis eet alleen plancton", 'Deze vis eet plancton en zeewier','Deze vis eet alleen aardappelen']
     if soort in vegetarischeVis:
         for vis in vegetarischeVis:
-            if soort == vis:
+            if soort in vis:
                 waarde = vegetarischeVis[soort]
                 print(soortVis[waarde])
-
 
 # Deze functie kijkt aan de hand van de meegegeven lengte, welke bak qua maat het beste aansluit op de vis
 def aqarium(lengte):
@@ -44,7 +43,6 @@ def aqarium(lengte):
         if maat == key:
             print("De lengte van de vis is geschikt voor", value)
 
-
-
-aqarium(5)
-vegetarisch('makreel')
+# vis1 = main('garnaal', 1500, 3,'roze')
+# vis2 = main('haaien', 3500, 5,'grijs')
+vis3 = main('haring', 500, 1,'groen')
