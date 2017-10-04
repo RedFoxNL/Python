baa9bb295b19945ef8908e94e5ebb1861c52c6f6
RedFoxNL <thom.boer@gmail.com>
2017-10-04 19:21:58 +0200

    fout in dubbele counter

---
+++ oefententamen/Opdracht2.py
@@ -0,0 +1,22 @@
+packets_str = ''
+with open('packet_example.txt', 'r') as f:
+    packets_str = f.read()
+
+packet_length=''
+mac_address =''
+packet_content=''
+packetstart = 0
+packetend = -1
+
+packet_list=[]
+
+for pointer in range(0,len(packets_str)):
+    if ....
+
+
+
+
+for packet in packet_list:
+    print(packet)
+
+
---
+++ oefententamen/opdracht3.py
@@ -0,0 +1,28 @@
+class Employee:
+
+
+
+
+
+
+class Management(Employee):
+
+
+
+
+
+company = []
+company.append(Employee('Johnson', 'sr. clerc', 'finance', 2300, False))
+company.append(Management('Bush jr.', 'manager', 'finance', 5400, 10, True))
+company.append(Employee('Jasons', 'clerc', 'finance', 1900, False))
+company.append(Management('Bushes', 'manager', 'R&D', 5400, 10, True))
+company.append(Employee('Hunter', 'researcher', 'R&D', 2300, False))
+company.append(Employee('Vries', 'researcher', 'R&D', 2300, False))
+company.append(Employee('Locker', 'researcher', 'R&D', 2300, False))
+company.append(Management('Schneier', 'manager', 'R&D', 6400, 10, True))
+print(Management.managers, Employee.employees)
+
+
+for employee in company:
+    print(employee)
+
