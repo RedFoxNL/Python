d98903fa9e606846a2861749af1b0b2153f2f312
Thom Boer <thom.boer@gmail.com>
2017-11-12 15:17:27 +0100

    opdrachten verbeterd en aangepast. Vooral Indexing was het probleem

--- tentamen oefening/opgave_1.py
+++ tentamen oefening/opgave_1.py
@@ -1,29 +1,37 @@
-blue='greenpark picadilly coventgarden holborn russellsquare kingscross'
-yellow='towerhill bakerstreet holborn blackfriars temple'
-red='bank holborn oxford bondstreet marblearch'
-brown='bakerstreet oxford picadilly charingcross'
+blue ='greenpark picadilly coventgarden holborn russellsquare kingscross'
+yellow ='towerhill bakerstreet holborn blackfriars temple'
+red ='bank holborn oxford bondstreet marblearch'
+brown ='bakerstreet oxford picadilly charingcross'
 lines_london = [blue, yellow, red, brown]
+
 
 # (a)
 set_of_stations = set()
-blue= (blue.split(" "))
-yellow= (yellow.split(" "))
-red= (red.split(" "))
-brown= (brown.split(" "))
+for line in lines_london:
+    station = line.split()
+    for s in station:
+        set_of_stations.add(s)
 
-set_of_stations = blue, yellow, red, brown
 
 print(set_of_stations)
 
 # (b)
 dict_of_stations = {}
-for stations in set_of_stations:
-    for stops in stations:
-        dict_of_stations[stops] = stations
-print(dict_of_stations)
+for s in set_of_stations:
+    neighbors = []
+    for line in lines_london:
+        stations = line.split()
+        if s in line:
+            i = stations.index(s)
+            if i > 0:
+                neighbors.append(stations[i-1])
+            if i < len(lines_london) -1:
+                neighbors.append(stations[i+1])
+    dict_of_stations[s] = neighbors
+
+for k,v in dict_of_stations.items():
+    print(k,v)
+
 
 # (c)
-print(max(dict_of_stations.keys()) ,max(dict_of_stations.values()))
-
-
-    
+print('station met de meeste buren:', max(dict_of_stations, key=lambda k:len(dict_of_stations[k])))
